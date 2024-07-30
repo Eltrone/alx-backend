670247ff5b93b280e457afa7ee1c569e7eca84d8
Eltrone <amineelorche@gmail.com>
2024-07-30 12:33:03 +0000

    file modified

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 """
-Ce module configure Flask et Babel pour une application multilingue avancée,
-sélectionnant la langue basée sur les préférences de l'utilisateur.
+Initialisation d'une application Flask avec Babel pour
+la sélection de langue basée sur la requête de l'utilisateur.
 """
 
 from flask import Flask, render_template, request
 from flask_babel import Babel, localeselector
+from typing import Optional
 
 app = Flask(__name__)
 
 
 class Config:
-    """ Configuration de base pour l'application Flask utilisant Babel. """
+    """
+    Configuration de Babel avec les langues et paramètres par défaut.
+    """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -22,14 +25,18 @@ babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
-    """ Sélect best langue disponible en fonction de la requête """
+def get_locale() -> Optional[str]:
+    """
+    Sélectionne la locale adaptée à partir de la requête HTTP.
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
-def index():
-    """ Affiche la page d'accueil. """
+def index() -> str:
+    """
+    Affiche la page d'accueil.
+    """
     return render_template('2-index.html')
 
 
