39502a8f6c01fc95723ec547d4005ba22d9f84e5
Eltrone <amineelorche@gmail.com>
2024-08-28 21:21:55 +0000

    file added

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -6,25 +6,25 @@ function createPushNotificationsJobs(jobs, queue) {
     }
 
     jobs.forEach(jobData => {
-        const job = queue.create('push_notification_code_3', jobData)
-            .save((err) => {
-                if (err) {
-                    console.error('Failed to create job', err);
-                } else {
-                    console.log(`Notification job created: ${job.id}`);
-                }
-            });
+        const job = queue.create('push_notification_code_3', jobData);
+        job.save((err) => {
+            if (err) {
+                console.error('Failed to create job', err);
+            } else {
+                console.log(`Notification job created: ${job.id}`);
 
-        job.on('complete', () => {
-            console.log(`Notification job ${job.id} completed`);
-        });
+                job.on('complete', () => {
+                    console.log(`Notification job ${job.id} completed`);
+                });
 
-        job.on('failed', (err) => {
-            console.log(`Notification job ${job.id} failed: ${err}`);
-        });
+                job.on('failed', (err) => {
+                    console.log(`Notification job ${job.id} failed: ${err}`);
+                });
 
-        job.on('progress', (progress) => {
-            console.log(`Notification job ${job.id} ${progress}% complete`);
+                job.on('progress', (progress) => {
+                    console.log(`Notification job ${job.id} ${progress}% complete`);
+                });
+            }
         });
     });
 }
--- 0x03-queuing_system_in_js/8-job.test.js
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -1,4 +1,5 @@
 // 8-job.test.js
+
 import chai from 'chai';
 import kue from 'kue';
 import createPushNotificationsJobs from './8-job.js';
@@ -22,7 +23,7 @@ describe('createPushNotificationsJobs', function() {
         expect(() => createPushNotificationsJobs({}, queue)).to.throw(Error, 'Jobs is not an array');
     });
 
-    it('should create two new jobs to the queue', function() {
+    it('should create two new jobs to the queue', function(done) {
         const jobs = [
             { phoneNumber: '4153518780', message: 'This is the code 1234 to verify your account' },
             { phoneNumber: '4153518781', message: 'This is the code 4562 to verify your account' }
@@ -30,9 +31,12 @@ describe('createPushNotificationsJobs', function() {
 
         createPushNotificationsJobs(jobs, queue);
 
-        expect(queue.testMode.jobs.length).to.equal(2);
-        expect(queue.testMode.jobs[0].type).to.equal('push_notification_code_3');
-        expect(queue.testMode.jobs[0].data).to.eql(jobs[0]);
-        expect(queue.testMode.jobs[1].data).to.eql(jobs[1]);
+        setTimeout(() => {
+            expect(queue.testMode.jobs.length).to.equal(2);
+            expect(queue.testMode.jobs[0].type).to.equal('push_notification_code_3');
+            expect(queue.testMode.jobs[0].data).to.eql(jobs[0]);
+            expect(queue.testMode.jobs[1].data).to.eql(jobs[1]);
+            done();
+        }, 50); // Delay to allow async operations to complete
     });
 });
